166d94e6bf30e82dd49067abd6ed1c6024aa626e
1lya_kovalenko <kovalenkoilyaden@gmail.com>
2024-12-09 15:11:42 +0300

    Initial commit

---
+++ src/main/java/com/java_chill_guys/MemoryTrainng/MemoryTrainngApplication.java
@@ -0,0 +1,13 @@
+package com.java_chill_guys.MemoryTrainng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryTrainngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryTrainngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_chill_guys/MemoryTrainng/MemoryTrainngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_chill_guys.MemoryTrainng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryTrainngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
