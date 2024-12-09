098114dedfc7cb764dae73b51ed536adb941f37e
1lya_kovalenko <kovalenkoilyaden@gmail.com>
2024-12-10 00:44:37 +0300

    DONE docker file. To use it you need to download docker desktop, then in intellij terminal exequte docker-compose up -d

---
+++ src/main/resources/db-init/V1_init_db.sql
@@ -0,0 +1,21 @@
+-- Шаг 1. Создание таблицы, если её ещё нет
+CREATE TABLE IF NOT EXISTS words
+(
+    id   BIGSERIAL PRIMARY KEY, -- Автоматическая генерация ID
+    word VARCHAR(255) NOT NULL  -- Колонка для слова
+);
+
+-- Шаг 2. Очистка таблицы перед загрузкой (опционально)
+TRUNCATE TABLE words;
+
+-- Шаг 3. Загрузка данных из файла (ид автоматически генерируется)
+COPY words (word)
+    FROM '/data/russian_words.txt'
+    WITH (
+    FORMAT TEXT, -- Текстовый формат файла
+    ENCODING 'UTF8' -- Кодировка файла
+    );
+
+DELETE
+FROM words w
+WHERE LENGTH(w.word) NOT BETWEEN 5 AND 8
